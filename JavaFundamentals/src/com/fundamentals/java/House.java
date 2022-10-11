d09ba1a10e9ee955301a2a0afd627d09eea12dd5
YonathanW <wubishetyonathan@gmail.com>
2022-10-10 20:53:12 -0500

    Lesson 2 -  My Application Overview

---
+++ JavaFundamentals/src/com/fundamentals/java/House.java
@@ -0,0 +1,14 @@
+package com.fundamentals.java;
+
+public class House {
+    public String foundationType;
+    public String roofStyle;
+    public String doorColor;
+    public int windowSize;
+
+    public void doorFunction() {
+        System.out.println("This door opens.");
+    }
+
+    // TODO: Add encapsulation to this class
+};
--- JavaFundamentals/src/com/fundamentals/java/Main.java
+++ JavaFundamentals/src/com/fundamentals/java/Main.java
@@ -1,9 +1,38 @@
+/*******************************
+  My Project
+ ********************************/
 package com.fundamentals.java;
 
+// main class
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+//        System.out.println("Hello Java\n");
+
+//        houseExample();
+        basicLab();
     } // end method
 
+    public static void basicLab() {
+        System.out.println("Hello Java\n");
+    }
+    /**
+     * houseExample method
+     */
+    public static void houseExample() {
+        House myHouse = new House();
+        myHouse.doorColor = "Red";
+        myHouse.doorFunction();
+
+        House otherHouse = new House();
+        otherHouse.doorColor = "Yellow";
+        otherHouse.doorFunction();
+
+        House thirdHouse = otherHouse;
+        thirdHouse.doorColor = "Blue";
+
+        System.out.println(myHouse.doorColor);
+        System.out.println(otherHouse.doorColor);
+        System.out.println(thirdHouse.doorColor);
+    }
 } // end class
